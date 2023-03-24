b28b6d4d0fc2df9665831584d2c619116338598d
Orkun Coskun <coskunorkun1@gmail.com>
2023-03-24 13:48:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Notları giriniz: ");
+        System.out.println("Matematik: ");
+        int mat = sc.nextInt();
+        System.out.println("Fizik: ");
+        int fizik = sc.nextInt();
+        System.out.println("Kimya: ");
+        int kimya = sc.nextInt();
+        System.out.println("Türkçe: ");
+        int türkce = sc.nextInt();
+        System.out.println("Tarih: ");
+        int tarih = sc.nextInt();
+        System.out.println("Müzik: ");
+        int muzik = sc.nextInt();
+
+        System.out.println("Ortalama: ");
+        double ortalama = (mat + fizik + kimya + türkce + tarih + muzik) / 6;
+        System.out.println(ortalama);
+    }
+}
