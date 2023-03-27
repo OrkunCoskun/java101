62ed9be8d097f2a959feb2897d519713d1733ec3
Orkun Coskun <coskunorkun1@gmail.com>
2023-03-27 18:01:37 +0300

    second assignment

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,44 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        //KDV Tutarı Hesaplayan Program
+
         Scanner sc = new Scanner(System.in);
+        double para, kdvOran, kdvli;
+        System.out.println("Para giriniz: ");
+        para = sc.nextInt();
+        if(para <= 1000 && para >= 0) {
+            kdvOran = 0.18;
+        }
+        else
+            kdvOran = 0.08;
 
-        System.out.println("Notları giriniz: ");
-        System.out.println("Matematik: ");
-        int mat = sc.nextInt();
-        System.out.println("Fizik: ");
-        int fizik = sc.nextInt();
-        System.out.println("Kimya: ");
-        int kimya = sc.nextInt();
-        System.out.println("Türkçe: ");
-        int türkce = sc.nextInt();
-        System.out.println("Tarih: ");
-        int tarih = sc.nextInt();
-        System.out.println("Müzik: ");
-        int muzik = sc.nextInt();
+        kdvli = para + (para * kdvOran);
+        System.out.println("KDV'siz Fiyat= " + para);
+        System.out.println("KDV'li Fiyat = " + kdvli);
+        System.out.println("KDV Oranı = " + kdvOran);
 
-        System.out.println("Ortalama: ");
-        double ortalama = (mat + fizik + kimya + türkce + tarih + muzik) / 6;
-        System.out.println(ortalama);
-        String sonuc = (ortalama>60) ? "Sınıfı Geçti" : "Sınıfta Kaldı";
-        System.out.println(sonuc);
+        //NOT Hesaplayan Program
+
+//        System.out.println("Notları giriniz: ");
+//        System.out.println("Matematik: ");
+//        int mat = sc.nextInt();
+//        System.out.println("Fizik: ");
+//        int fizik = sc.nextInt();
+//        System.out.println("Kimya: ");
+//        int kimya = sc.nextInt();
+//        System.out.println("Türkçe: ");
+//        int türkce = sc.nextInt();
+//        System.out.println("Tarih: ");
+//        int tarih = sc.nextInt();
+//        System.out.println("Müzik: ");
+//        int muzik = sc.nextInt();
+//
+//        System.out.println("Ortalama: ");
+//        double ortalama = (mat + fizik + kimya + türkce + tarih + muzik) / 6;
+//        System.out.println(ortalama);
+//        String sonuc = (ortalama>60) ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+//        System.out.println(sonuc);
     }
 }
