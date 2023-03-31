77a150b23e648bedac24151d72da41d8694fe955
Orkun Coskun <coskunorkun1@gmail.com>
2023-03-31 15:30:02 +0300

    ifelse assignment 2

--- src/ifelse/Main.java
+++ src/ifelse/Main.java
@@ -5,42 +5,75 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //Basit Hesap Makinesi
+        //Kullanıcı Girişi
 
-        int n1,n2,select;
+        String username, password,answer, newPassword;
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Birinci Sayıyı Giriniz: ");
-        n1 = sc.nextInt();
-        System.out.println("İkinci Sayıyı Giriniz: ");
-        n2 = sc.nextInt();
+        System.out.println("Kullanıcı adınız: ");
+        username = sc.nextLine();
 
-        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
-        System.out.println("Seçiminiz: ");
-        select = sc.nextInt();
+        System.out.println("Şifreniz: ");
+        password = sc.nextLine();
 
-        switch (select) {
-            case 1:
-                System.out.println("Toplam: " + (n1 + n2) );
-                break;
-            case 2:
-                System.out.println("Çıkarma: " + (n1 - n2) );
-                break;
-            case 3:
-                System.out.println("Çarpım: " + (n1 * n2) );
-                break;
-            case 4:
-                if (n2 != 0){
-                    System.out.println("Bölüm: " + (n1 / n2));
-                }
-                else {
-                    System.out.println("Bir sayı 0'a bölünemez!");
-                }
-                break;
-            default:
-                System.out.println("Hata");
-
+        if(username.equals("patika") && password.equals("java123")) {
+            System.out.println("Giriş Yaptınız!");
         }
+        else {
+            System.out.println("Bilgileriniz Yanlış! Şifrenizi sıfırlamak istiyor musunuz?");
+            answer = sc.nextLine();
+            if(answer.equals("evet")) {
+                System.out.println("Yeni şifreyi giriniz: ");
+                newPassword = sc.nextLine();
+                if(newPassword.equals(password) || newPassword.equals("java123")) {
+                    System.out.println("Şifre oluşturulamadı, lütfen başka şifre giriniz.");
+                }
+                else
+                    System.out.println("Şifre oluşturuldu!");
+            }
+            else
+                System.out.println("Sistemden çıkıldı");
+        }
+
+
+
+        //Basit Hesap Makinesi
+
+//        int n1,n2,select;
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Birinci Sayıyı Giriniz: ");
+//        n1 = sc.nextInt();
+//        System.out.println("İkinci Sayıyı Giriniz: ");
+//        n2 = sc.nextInt();
+//
+//        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+//        System.out.println("Seçiminiz: ");
+//        select = sc.nextInt();
+//
+//        switch (select) {
+//            case 1:
+//                System.out.println("Toplam: " + (n1 + n2) );
+//                break;
+//            case 2:
+//                System.out.println("Çıkarma: " + (n1 - n2) );
+//                break;
+//            case 3:
+//                System.out.println("Çarpım: " + (n1 * n2) );
+//                break;
+//            case 4:
+//                if (n2 != 0){
+//                    System.out.println("Bölüm: " + (n1 / n2));
+//                }
+//                else {
+//                    System.out.println("Bir sayı 0'a bölünemez!");
+//                }
+//                break;
+//            default:
+//                System.out.println("Hata");
+//
+//        }
 
 //        if(select == 1) {
 //            System.out.println("Toplam: " + (n1 + n2) );
