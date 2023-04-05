02fa60ca393698b4d30eb08bc0d4f0a6f8f60cd4
Orkun Coskun <coskunorkun1@gmail.com>
2023-04-05 16:03:49 +0300

    loops assignment9

--- src/loops/Main.java
+++ src/loops/Main.java
@@ -5,32 +5,105 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //ATM Projesi
+
+        String userName, password;
+        int right = 3;
+        int balance = 1500;
+        int select;
+        Scanner sc = new Scanner(System.in);
+
+        while (right > 0) {
+            System.out.print("Kullanıcı Adınız: ");
+            userName = sc.nextLine();
+            System.out.print("Parolanız: ");
+            password = sc.nextLine();
+            if (userName.equals("patika") && password.equals("dev123")) {
+                System.out.println("Merhaba, Kodluyoruz Bankasına Hoşgeldiniz!");
+                do {
+                    System.out.println("1- Para Yatırma\n" +
+                            "2- Para Çekme\n" +
+                            "3- Bakiye Sorgula\n" +
+                            "4- Çıkış Yap");
+                    System.out.print("Lütfen yapmak istediğiniz işlemi seçiniz: ");
+                    select = sc.nextInt();
+                    int price;
+                    switch (select) {
+                        case 1:
+                            System.out.print("Para miktarı: ");
+                            price = sc.nextInt();
+                            balance += price;
+                            break;
+                        case 2:
+                            System.out.print("Para miktarı: ");
+                            price = sc.nextInt();
+                            if (price > balance) {
+                                System.out.println("Bakiye yetersiz.");
+                            } else {
+                                balance -= price;
+                            }
+                            break;
+                        case 3:
+                            System.out.println("Bakiyeniz : " + balance);
+                            break;
+                    }
+//                    if (select == 1) {
+//                        System.out.print("Para miktarı: ");
+//                        int price = sc.nextInt();
+//                        balance += price;
+//                    }
+//                    else if (select == 2) {
+//                        System.out.print("Para miktarı: ");
+//                        int price = sc.nextInt();
+//                        if (price > balance) {
+//                            System.out.println("Bakiye yetersiz.");
+//                        }
+//                        else {
+//                            balance -= price;
+//                        }
+//                    }
+//                    else if (select == 3) {
+//                        System.out.println("Bakiyeniz : " + balance);
+//                    }
+                } while (select != 4);
+                System.out.println("Tekrar görüşmek üzere!");
+                break;
+            } else {
+                right--;
+                System.out.println("Hatalı kullanıcı adı veya şifre. Tekrar deneyiniz.");
+                if (right == 0) {
+                    System.out.println("Hesabınız bloke olmuştur lüfen banka ile iletişime geçiniz.");
+                } else {
+                    System.out.println("Kalan Hakkınız: " + right);
+                }
+            }
+        }
+
+
         //Yıldızlar ile Üçgen Yapımı
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Basamak sayısını giriniz: ");
-        int n = sc.nextInt();
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= (n-i); j++) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= (2*i) - 1; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for (int x = n - 1; x >= 0; x--) {
-            for (int j = 1; j <= (n - x); j++) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= (2 * x) - 1; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
-
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Basamak sayısını giriniz: ");
+//        int n = sc.nextInt();
+//
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= (n-i); j++) {
+//                System.out.print(" ");
+//            }
+//            for (int k = 1; k <= (2*i) - 1; k++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for (int x = n - 1; x >= 0; x--) {
+//            for (int j = 1; j <= (n - x); j++) {
+//                System.out.print(" ");
+//            }
+//            for (int k = 1; k <= (2 * x) - 1; k++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
 
 
         //Harmonik Sayıları Bulan Program
@@ -44,7 +117,6 @@ public class Main {
 //            result += (1/i);
 //        }
 //        System.out.println(result);
-
 
 
         //Bir Sayının Basamak Sayılarının Toplamını Hesaplayan Program
@@ -135,7 +207,6 @@ public class Main {
 //        System.out.println(result);
 
 
-
         //Üslü Sayı Hesaplayan Program
 
         //with for loop
@@ -199,7 +270,6 @@ public class Main {
 //        System.out.println(n + ". Faktöriyel: " + total);
 
 
-
         //Girilen Sayıdan Küçük 4 ve 5'in Kuvvetlerini Bulan Program
 
 //        int n;
@@ -256,7 +326,6 @@ public class Main {
 //        System.out.println("Toplam: " + sum);
 
 
-
         //Girilen Sayıya Kadar Olan 3 ve 4'e Tam Bölünen Sayıların Ortalamasını Hesaplayan Program
 
 //        int k,sum=0,count=0;
@@ -274,8 +343,6 @@ public class Main {
 //        }
 //        double average = (double) sum / count;
 //        System.out.println("Ortalama: " + average);
-
-
 
 
         //Girilen Sayıya Kadar Olan Çift Sayıları Bulan Program
