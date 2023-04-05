f56df5b7d35b64804611408078f3f7666415f65f
Orkun Coskun <coskunorkun1@gmail.com>
2023-04-05 12:57:58 +0300

    loops assignment8

--- src/loops/Main.java
+++ src/loops/Main.java
@@ -5,19 +5,45 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //Harmonik Sayıları Bulan Program
+        //Yıldızlar ile Üçgen Yapımı
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("N sayısını giriniz: ");
+        System.out.print("Basamak sayısını giriniz: ");
         int n = sc.nextInt();
-        double result = 0;
 
-        for (double i = 1; i <= n; i++) {
-            result += (1/i);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n-i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2*i) - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        System.out.println(result);
+        for (int x = n - 1; x >= 0; x--) {
+            for (int j = 1; j <= (n - x); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * x) - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
 
+
+
+        //Harmonik Sayıları Bulan Program
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("N sayısını giriniz: ");
+//        int n = sc.nextInt();
+//        double result = 0;
+//
+//        for (double i = 1; i <= n; i++) {
+//            result += (1/i);
+//        }
+//        System.out.println(result);
 
 
 
