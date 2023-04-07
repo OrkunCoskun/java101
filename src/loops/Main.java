2a6be5fe1006bd8dc9fe58e5ee6748cd9c355477
Orkun Coskun <coskunorkun1@gmail.com>
2023-04-07 15:39:39 +0300

    loops homework4

--- src/loops/Main.java
+++ src/loops/Main.java
@@ -5,20 +5,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //Ters Üçgen Yapımı
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Bir Sayı Giriniz: ");
-        int n = sc.nextInt();
+        //1-100 Arasındaki Asal Sayıları Bulan Program
 
-        for (int i = n - 1; i >= 1; i--) {
-            for (int j = 1; j <= (n - i); j++) {
-                System.out.print(" ");
+        int sayi;
+        for(int i=2; i<=100; i++){
+            sayi=0;
+            for(int j=2; j<=i/2; j++){
+                if(i%j==0)
+                    sayi++;
             }
-            for (int k = 1; k <= (2 * i) - 1; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
+            if(sayi==0)
+                System.out.print(+i + " ");
         }
+
+
+        //Ters Üçgen Yapımı
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Bir Sayı Giriniz: ");
+//        int n = sc.nextInt();
+//
+//        for (int i = n - 1; i >= 1; i--) {
+//            for (int j = 1; j <= (n - i); j++) {
+//                System.out.print(" ");
+//            }
+//            for (int k = 1; k <= (2 * i) - 1; k++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
 
         //Mükemmel Sayı Bulan Program
 
