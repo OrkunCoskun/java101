19bdc0ba093e7dae5c1604fb377bcccf684f93d2
Orkun Coskun <coskunorkun1@gmail.com>
2023-04-12 14:43:59 +0300

    methods assignment2

--- src/methods/Main.java
+++ src/methods/Main.java
@@ -1,6 +1,21 @@
 package methods;
 
+import java.util.Scanner;
+
 public class Main {
+
+    //Recursive ile Fibonacci Serisi Bulan Program
+
+    static int fib(int n) {
+
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+
+    }
+
+    //Palindrom Sayılar
 
     static boolean isPalindrome(int number) {
         int temp = number, reverseNumber = 0, lastDigit;
@@ -15,9 +30,108 @@ public class Main {
             return false;
     }
 
+    static int sum (int a, int b) {
+        int result = a + b;
+        System.out.println("Toplam: " + result);
+        return result;
+    }
+
+    static int minus (int a, int b) {
+        int result = a - b;
+        System.out.println("Çıkarma: " + result);
+        return result;
+    }
+
+    static int times (int a, int b) {
+        int result = a * b;
+        System.out.println("Çarpım: " + result);
+        return result;
+    }
+
+    static int divided (int a, int b) {
+        if (b == 0) {
+            System.out.println("İkinci sayı 0'dan farklı olmalıdır!");
+            return 0;
+        }
+        int result = a / b;
+        System.out.println("Bölüm : " + result);
+        return  result;
+    }
+
+    static int power(int a, int b) {
+        int result = 1;
+        for (int i = 1; i <= b; i++) {
+            result *= a;
+        }
+        return result;
+    }
+
+    static int mod(int a, int b) {
+        return a % b;
+    }
+
+    static void calc(int a, int b) {
+        System.out.println("Çevresi: " + (2 * (a + b)));
+        System.out.println("Alanı: " + (a * b));
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(isPalindrome(4004));
+//        System.out.println(isPalindrome(101));
+//        System.out.println(fib(9));
+
+        Scanner sc = new Scanner(System.in);
+        int select;
+
+        String menu = "1- Toplama İşlemi\n"
+                + "2- Çıkarma İşlemi\n"
+                + "3- Çarpma İşlemi\n"
+                + "4- Bölme İşlemi\n"
+                + "5- Üslü Sayı Hesaplama\n"
+                + "6- Mod Alma\n"
+                + "7- Dikdörtgen Alan ve Çevre Hesabı\n"
+                + "0- Çıkış Yap\n";
+
+        System.out.println(menu);
+        while (true) {
+            System.out.print("Bir İşlem Seçiniz: ");
+            select = sc.nextInt();
+
+            if (select == 0)
+                break;
+
+            System.out.print("İlk Sayı: ");
+            int a = sc.nextInt();
+            System.out.print("İkinci Sayı: ");
+            int b = sc.nextInt();
+
+            switch (select) {
+                case 1:
+                    sum(a, b);
+                    break;
+                case 2:
+                    minus(a, b);
+                    break;
+                case 3:
+                    times(a, b);
+                    break;
+                case 4:
+                    divided(a, b);
+                    break;
+                case 5:
+                    System.out.println("Üs Hesabı: " + power(a, b));
+                    break;
+                case 6:
+                    System.out.println("Mod İşlemi: " + mod(a, b));
+                    break;
+                case 7:
+                    calc(a, b);
+                    break;
+                default:
+                    System.out.println("Geçersiz bir işlem girdiniz!");
+            }
+        }
+        System.out.println("Güle Güle");
 
     }
 }
