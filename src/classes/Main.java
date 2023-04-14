d0d5949d3e9fd3b5c62f1a851f35e3ffa14f03b8
Orkun Coskun <coskunorkun1@gmail.com>
2023-04-14 15:35:15 +0300

    classes assignment1

--- src/classes/Main.java
+++ src/classes/Main.java
@@ -3,19 +3,51 @@ package classes;
 public class Main {
     public static void main(String[] args) {
 
-        Car audi = new Car("Sports", "Audi", "red");
-        audi.increaseSpeed(20);
-        audi.printInfo();
+        //Öğrenci Bilgi Sistemi
 
-        Car bmw = new Car("Sports" , "BMW" , "blue");
-        bmw.increaseSpeed(10);
-        bmw.increaseSpeed(25);
-        bmw.increaseSpeed(5);
-        bmw.decreaseSpeed(25);
-        bmw.printInfo();
+        Teacher t1 = new Teacher("Mahmut Hoca","555","TRH");
+        Teacher t2 = new Teacher("Graham Bell","0000","FZK");
+        Teacher t3 = new Teacher("Külyutmaz", "111", "BIO");
 
-        Car mercedes = new Car("Corporate" , "Mercedes" , "black");
-        mercedes.speed = 20;
-        mercedes.printInfo();
+        Course tarih = new Course("Tarih","101","TRH");
+        tarih.addTeacher(t1);
+
+        Course fizik = new Course("Fizik","102","FZK");
+        fizik.addTeacher(t2);
+
+        Course biyo = new Course("Biyoloji","101","BIO");
+        biyo.addTeacher(t3);
+
+
+        Student s1 = new Student("İnek Şaban", "123", "4", tarih, fizik, biyo);
+        s1.addBulkExamNote(100,78,50);
+        s1.isPass();
+
+        Student s2 = new Student("Güdük Necmi", "444", "4", tarih, fizik, biyo);
+        s2.addBulkExamNote(50,30,70);
+        s2.isPass();
+
+
+
+
+
+
+
+
+
+//        Car audi = new Car("Sports", "Audi", "red");
+//        audi.increaseSpeed(20);
+//        audi.printInfo();
+//
+//        Car bmw = new Car("Sports" , "BMW" , "blue");
+//        bmw.increaseSpeed(10);
+//        bmw.increaseSpeed(25);
+//        bmw.increaseSpeed(5);
+//        bmw.decreaseSpeed(25);
+//        bmw.printInfo();
+//
+//        Car mercedes = new Car("Corporate" , "Mercedes" , "black");
+//        mercedes.speed = 20;
+//        mercedes.printInfo();
     }
 }
