8e88d8ee1c6fae081384bf6fb792c2312ffaa122
Orkun Coskun <coskunorkun1@gmail.com>
2023-04-14 14:52:49 +0300

    classes constructor

--- src/classes/Car.java
+++ src/classes/Car.java
@@ -9,20 +9,35 @@ class Car {
     int speed;
     int speedLimit = 180;
 
+    // Constructor (Kurucu) Metot
+    Car(String type, String model, String color) {
+        this.type = type;
+        this.model = model;
+        this.color = color;
+        this.speed = 0;
+    }
+
     // Davranışlar
 
     void increaseSpeed(int increment) {
-        if ((speed + increment) < speedLimit)
-            speed += increment;
+        this.speed += increment;
     }
 
     void decreaseSpeed(int decrease) {
-        if (speed > 0 )
-            speed -= decrease;
+        if (this.speed > 0 )
+            this.speed -= decrease;
     }
 
     void printSpeed() {
-        System.out.println("Hızınız: " + speed);
+        System.out.println("Hızınız: " + this.speed);
+    }
+
+    void printInfo() {
+        System.out.println("================");
+        System.out.println("Model  : " + this.model);
+        System.out.println("Color  : " + this.color);
+        System.out.println("Type  : " + this.type);
+        System.out.println("Speed  : " + this.speed);
     }
 
 }
