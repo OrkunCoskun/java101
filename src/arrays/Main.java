bdcf6cd001485bf43c5a267e0e8780a815bc7bb9
Orkun Coskun <coskunorkun1@gmail.com>
2023-04-19 15:35:51 +0300

    arrays assignment2

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -1,22 +1,75 @@
 package arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        //Dizideki Elemanların Girilen Sayıdan Küçük En Yakın Sayıyı ve En Büyük En Yakın Sayıyı Bulan Program
+
+        int[] list = {15, 12, 788, 1, -1, -778, 2, 0};
+
+        Arrays.sort(list);
+        int minClosed = 0;
+        int maxClosed = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Değer giriniz: ");
+        int value = sc.nextInt();
+
+        for (int i : list) {
+            if (i < value && i > minClosed) {
+                minClosed = i;
+            }
+        }
+
+        for (int i : list) {
+            if (i > value) {
+                maxClosed = i;
+                break;
+            }
+        }
+
+        System.out.println("En yakın minimum değer: " + minClosed);
+        System.out.println("En yakın maksimum değer: " + maxClosed);
+
+
+        //Dizideki Maksimum ve Minimum Değerleri Bulan Program
+
+//        int[] list = {56, 34, 1, 8, 101, -2, -33};
+//
+//        int min = list[0];
+//        int max = list[0];
+//
+//        for (int i : list) {
+//            if (i < min) {
+//                min = i;
+//            }
+//
+//            if (i > max) {
+//                max = i;
+//            }
+//        }
+//
+//        System.out.println("Minimum Değer: " + min);
+//        System.out.println("Maximum Değer: " + max);
+
+
         //Dizideki Sayıların Harmonik Ortalamasını Hesaplayan Program
 
-        int[] numbers = {1, 2, 3, 4, 5, 6};
-
-        double sum = 0.0;
-
-        for (int i = 0; i < numbers.length; i++) {
-            sum += 1.0 / numbers[i];
-        }
-        System.out.println(sum);
-
-        double average = numbers.length / sum;
-
-        System.out.println(average);
+//        int[] numbers = {1, 2, 3, 4, 5, 6};
+//
+//        double sum = 0.0;
+//
+//        for (int i = 0; i < numbers.length; i++) {
+//            sum += 1.0 / numbers[i];
+//        }
+//        System.out.println(sum);
+//
+//        double average = numbers.length / sum;
+//
+//        System.out.println(average);
 
 
         //Dizideki Elemanların Ortalamasını Bulan Program
