c7e0e535fc6df0c8584678c3d9bbad1f1aadf1a8
Orkun Coskun <coskunorkun1@gmail.com>
2023-04-19 15:50:36 +0300

    arrays assignment3

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -6,33 +6,58 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //Çok Boyutlu Diziler ile B Harfi Yazdıran Program
+
+        String[][] letter = new String[6][4];
+
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (i == 0 || i == 2 || i == 5) {
+                    letter[i][j] = " * ";
+                }
+                else if (j == 0 || j == 3) {
+                    letter[i][j] = " * ";
+                }
+                else
+                    letter[i][j] = "   ";
+            }
+        }
+
+        for (String[] row: letter) {
+            for (String col: row) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+
+
         //Dizideki Elemanların Girilen Sayıdan Küçük En Yakın Sayıyı ve En Büyük En Yakın Sayıyı Bulan Program
 
-        int[] list = {15, 12, 788, 1, -1, -778, 2, 0};
-
-        Arrays.sort(list);
-        int minClosed = 0;
-        int maxClosed = 0;
-
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Değer giriniz: ");
-        int value = sc.nextInt();
-
-        for (int i : list) {
-            if (i < value && i > minClosed) {
-                minClosed = i;
-            }
-        }
-
-        for (int i : list) {
-            if (i > value) {
-                maxClosed = i;
-                break;
-            }
-        }
-
-        System.out.println("En yakın minimum değer: " + minClosed);
-        System.out.println("En yakın maksimum değer: " + maxClosed);
+//        int[] list = {15, 12, 788, 1, -1, -778, 2, 0};
+//
+//        Arrays.sort(list);
+//        int minClosed = 0;
+//        int maxClosed = 0;
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Değer giriniz: ");
+//        int value = sc.nextInt();
+//
+//        for (int i : list) {
+//            if (i < value && i > minClosed) {
+//                minClosed = i;
+//            }
+//        }
+//
+//        for (int i : list) {
+//            if (i > value) {
+//                maxClosed = i;
+//                break;
+//            }
+//        }
+//
+//        System.out.println("En yakın minimum değer: " + minClosed);
+//        System.out.println("En yakın maksimum değer: " + maxClosed);
 
 
         //Dizideki Maksimum ve Minimum Değerleri Bulan Program
