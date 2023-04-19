8a12d22615dc35fa0f135eeb1079ab20482d4acc
Orkun Coskun <coskunorkun1@gmail.com>
2023-04-19 22:01:28 +0300

    arrays homework1 edited

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -15,6 +15,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Dizideki Elemanları Küçükten Büyüğe Sıralayan Program
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Dizinin boyutu n:");
