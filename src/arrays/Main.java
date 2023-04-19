de3f1ac2331dd604ee54e9e8dda36ee408da00d9
Orkun Coskun <coskunorkun1@gmail.com>
2023-04-19 22:45:26 +0300

    arrays homework3

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -15,34 +15,58 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Çok Boyutlu Dizilerle Oluşturulmuş Matrisin Transpozunu Bulan Program
+
+        int[][] list = {{2, 3, 4}, {5, 6, 4}, {7,8,9}};
+        int[][] newList = new int[3][3];
+
+        System.out.println("Matris: ");
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list[i].length; j++) {
+                System.out.print(list[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Transpoze: ");
+        for(int i = 0; i < list.length; i++){
+            for(int j = 0; j < list[i].length; j++){
+                newList[i][j] = list[j][i];
+                System.out.print(newList[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+
+
         //Dizideki Elemanların Kaç Kez Tekrar Edildiğini Yani Frekanslarını Bulan Program
 
-        int[] list = {10, 20, 20, 10, 10, 20, 5, 20};
-
-        System.out.println("Dizi: " + Arrays.toString(list));
-        System.out.println("Tekrar Sayıları:");
-        Arrays.sort(list);
-
-        int counter = 1;
-
-        for (int i = 0; i < list.length; i++) {
-            if (i == 0) {
-                for (int j = 0; j < list.length; j++) {
-                    if ((i != j) && (list[i] == list[j])) {
-                        counter++;
-                    }
-                }
-                System.out.println(list[i] + " sayısı " + counter + " kere tekrar edildi.");
-            } else if (list[i] != list[i-1]) {
-                for (int j = 0; j < list.length; j++) {
-                    if ((i != j) && (list[i] == list[j])) {
-                        counter++;
-                    }
-                }
-                System.out.println(list[i] + " sayısı " + counter + " kere tekrar edildi.");
-            }
-            counter = 1;
-        }
+//        int[] list = {10, 20, 20, 10, 10, 20, 5, 20};
+//
+//        System.out.println("Dizi: " + Arrays.toString(list));
+//        System.out.println("Tekrar Sayıları:");
+//        Arrays.sort(list);
+//
+//        int counter = 1;
+//
+//        for (int i = 0; i < list.length; i++) {
+//            if (i == 0) {
+//                for (int j = 0; j < list.length; j++) {
+//                    if ((i != j) && (list[i] == list[j])) {
+//                        counter++;
+//                    }
+//                }
+//                System.out.println(list[i] + " sayısı " + counter + " kere tekrar edildi.");
+//            } else if (list[i] != list[i-1]) {
+//                for (int j = 0; j < list.length; j++) {
+//                    if ((i != j) && (list[i] == list[j])) {
+//                        counter++;
+//                    }
+//                }
+//                System.out.println(list[i] + " sayısı " + counter + " kere tekrar edildi.");
+//            }
+//            counter = 1;
+//        }
 
 
         //Dizideki Elemanları Küçükten Büyüğe Sıralayan Program
